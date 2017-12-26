5ec48711d44ce881e9c1aa00bc7453cdaf9c8b97
Aroun31 <mc.family3531@gmail.com>
2017-12-26 19:05:09 +0100

    MAJ SQL File

--- da3f_rpcoop.sql
+++ da3f_rpcoop.sql
@@ -5,6 +5,14 @@ SET time_zone = "+00:00";
 -- This SQL uses utf8mb4 and has CURRENT_TIMESTAMP function.
 --
 
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 26 Décembre 2017 à 18:53
+-- Version du serveur :  5.7.20-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -12,18 +20,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `daf_rpcoop`
+-- Database: `da3f_cprp`
 -- Default Schema
 --
-CREATE DATABASE IF NOT EXISTS `daf_rpcoop` DEFAULT CHARACTER SET utf8mb4;
-USE `daf_rpcoop`;
+CREATE DATABASE IF NOT EXISTS `da3f_cprp` DEFAULT CHARACTER SET utf8mb4;
+USE `da3f_cprp`;
 
 --
 -- Drop procedures to ensure no conflicts
 --
-DROP PROCEDURE IF EXISTS `resetLifeVehicles`;
-DROP PROCEDURE IF EXISTS `deleteDeadVehicles`;
-DROP PROCEDURE IF EXISTS `deleteOldContainers`;
+DROP PROCEDURE IF EXISTS `resetCpRpVehicles`;
+DROP PROCEDURE IF EXISTS `delDeadVeh`;
+DROP PROCEDURE IF EXISTS `deleteOldStock`;
 
 DELIMITER $$
 --
@@ -32,18 +40,18 @@ DELIMITER $$
 -- For external databases: Edit localhost to match arma3server IP
 --
 
-CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `resetLifeVehicles`()
+CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `resetCpRpVehicles`()
 BEGIN
   UPDATE `vehicles` SET `active`= 0;
 END$$
 
-CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `deleteDeadVehicles`()
+CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `delDeadVeh`()
 BEGIN
   DELETE FROM `vehicles` WHERE `alive` = 0;
 END$$
 
 
-CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `deleteOldContainers`()
+CREATE DEFINER=`Utilisateur`@`localhost` PROCEDURE `deleteOldStock`()
 BEGIN
   DELETE FROM `containers` WHERE `owned` = 0;
 END$$
@@ -53,17 +61,37 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `players`
+-- Structure de la table `containers`
 --
 
-CREATE TABLE IF NOT EXISTS `players` (
-  `uid` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `containers` (
+  `id` int(6) NOT NULL,
+  `pid` varchar(17) NOT NULL,
+  `classname` varchar(32) NOT NULL,
+  `pos` varchar(64) DEFAULT NULL,
+  `inventory` text NOT NULL,
+  `gear` text NOT NULL,
+  `dir` varchar(128) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `owned` tinyint(1) DEFAULT '0',
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `players`
+--
+
+CREATE TABLE `players` (
+  `uid` int(6) NOT NULL,
   `name` varchar(32) NOT NULL,
   `aliases` text NOT NULL,
   `pid` varchar(17) NOT NULL,
   `cash` int(100) NOT NULL DEFAULT '0',
+  `cashwar` int(100) NOT NULL DEFAULT '0',
   `bankacc` int(100) NOT NULL DEFAULT '0',
-  `hierarchie` enum('0','1','2','3','4','5','6','7') NOT NULL DEFAULT '0',
+  `hierarchie` enum('0','1','2','3','4','5','6','7','8','9') NOT NULL DEFAULT '0',
   `xplevel` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '0',
   `spec` text NOT NULL,
   `inv_virt` text NOT NULL,
@@ -73,64 +101,104 @@ CREATE TABLE IF NOT EXISTS `players` (
   `vip` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
   `blacklist` tinyint(1) NOT NULL DEFAULT '0',
   `save_pos` varchar(64) NOT NULL DEFAULT '"[]"',
+  `mdp_coms` int(20) NOT NULL DEFAULT '1234',
+  `mymsg` text NOT NULL,
   `playtime` varchar(32) NOT NULL DEFAULT '"[0,0,0]"',
-  `insert_time` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `pid` (`pid`),
-  KEY `name` (`name`),
-  KEY `blacklist` (`blacklist`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=12 ;
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vehicles`
+-- Structure de la table `stockage`
 --
 
-CREATE TABLE IF NOT EXISTS `vehicles` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stockage` (
+  `Name` varchar(32) NOT NULL,
+  `Items` text NOT NULL,
+  `Weapons` text NOT NULL,
+  `Magazines` text NOT NULL,
+  `Backpacks` text NOT NULL,
+  `VirtItems` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(6) NOT NULL,
   `side` varchar(16) NOT NULL,
   `classname` varchar(64) NOT NULL,
   `type` varchar(16) NOT NULL,
   `pid` varchar(17) NOT NULL,
   `alive` tinyint(1) NOT NULL DEFAULT '1',
   `active` tinyint(1) NOT NULL DEFAULT '0',
-  `plak` int(20) NOT NULL,
+  `plak` text NOT NULL,
   `color` int(20) NOT NULL,
   `inventory` text NOT NULL,
   `gear` text NOT NULL,
   `fuel` double NOT NULL DEFAULT '1',
   `damage` varchar(256) NOT NULL,
-  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `side` (`side`),
-  KEY `pid` (`pid`),
-  KEY `type` (`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
-
--- --------------------------------------------------------
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Table structure for table `containers`
--- Needed for extDB latest update on git
+-- Index pour les tables exportées
 --
 
-CREATE TABLE IF NOT EXISTS `containers` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `pid` varchar(17) NOT NULL,
-  `classname` varchar(32) NOT NULL,
-  `pos` varchar(64) DEFAULT NULL,
-  `inventory` text NOT NULL,
-  `gear` text NOT NULL,
-  `dir` varchar(128) DEFAULT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '0',
-  `owned` tinyint(1) DEFAULT '0',
-  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`,`pid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=4;
+--
+-- Index pour la table `containers`
+--
+ALTER TABLE `containers`
+  ADD PRIMARY KEY (`id`,`pid`);
 
--- --------------------------------------------------------
+--
+-- Index pour la table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `pid` (`pid`),
+  ADD KEY `name` (`name`),
+  ADD KEY `blacklist` (`blacklist`);
+
+--
+-- Index pour la table `stockage`
+--
+ALTER TABLE `stockage`
+  ADD PRIMARY KEY (`Name`);
+
+--
+-- Index pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `side` (`side`),
+  ADD KEY `pid` (`pid`),
+  ADD KEY `type` (`type`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `containers`
+--
+ALTER TABLE `containers`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `players`
+--
+ALTER TABLE `players`
+  MODIFY `uid` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+--
+-- AUTO_INCREMENT pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
